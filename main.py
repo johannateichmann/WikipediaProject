f994ea6d09774489d857125808b80b17d2137011
unknown <johannateichmann>
2019-05-05 13:57:03 +0200

    git config --global user.email "cerle750gmail.com" git config --global user.name "johannateichmann"

---
+++ main.py
@@ -0,0 +1,165 @@
+#importing relevant modules
+import sys
+import pandas as pd   #creating shortcut to panda module via nametag pd
+from qtpy import QtWidgets, QtCore   #using the qt-creator
+from ui.mainwindow import Ui_MainWindow
+import csv #module for comma seperated values(excel files)
+
+app = QtWidgets.QApplication(sys.argv)
+
+#loading data for operations and storing it in lists
+file = open("data/GeographicalEurope.csv", "r", newline="") #opens the file GeographicalEurope
+first = file.readline().strip().split(";")
+index=[]
+for i in range(0, 47):
+    index.append(i)
+countries =[]
+capitol =[]
+space =[]
+inhabit =[]
+inhabits_per_km =[]
+bip =[]
+bip_per_capita =[]
+region =[]
+
+for line in file:
+    countries.append(line.strip().split(";")[0])
+    capitol.append(line.strip().split(";")[1])
+    space.append(line.strip().split(";")[2])
+    inhabit.append(line.strip().split(";")[3])
+    inhabits_per_km.append(line.strip().split(";")[4])
+    bip.append(line.strip().split(";")[5])
+    bip_per_capita.append(line.strip().split(";")[6])
+    region.append(line.strip().split(";")[7])
+
+dp = pd.DataFrame({first[0]: countries,
+                    first[1]: capitol,
+                    first[2]: space,
+                    first[3]: inhabit,
+                    first[4]: inhabits_per_km,
+                    first[5]: bip,
+                    first[6]: bip_per_capita,
+                    first[7]: region},
+                    index)
+#country codes for easier handeling
+country_codes ={"Albania" : 0,
+                "Andorra" : 1,
+                "Austria" : 2,
+                "Belarus" : 3,
+                "Belgium" : 4,
+                "Bosnia and Herzegowina" : 5,
+                "Bulgaria" : 6,
+                "Croatia" : 7,
+                "Czech Republic" : 8,
+                "Denmark" : 9,
+                "Estonia" : 10,
+                "Finland" : 11,
+                "France" : 12,
+                "Germany" : 13,
+                "Greece" : 14,
+                "Hungary" : 15,
+                "Iceland" : 16,
+                "Ireland" : 17,
+                "Italy" : 18,
+                "Kazakhstan" : 19,
+                "Kosovo" : 20,
+                "Latvia" : 21,
+                "Liechtenstein" : 22,
+                "Lithuania" : 23,
+                "Luxembourg" : 24,
+                "Malta" : 25,
+                "Moldova" : 26,
+                "Monaco" : 27,
+                "Montenegro" : 28,
+                "Netherlands" : 29,
+                "Northern Macedonia" : 30,
+                "Norway" : 31,
+                "Poland" : 32,
+                "Portugal " : 33,
+                "Romania" : 34,
+                "Russia" : 35,
+                "San Marino" : 36,
+                "Serbia" : 37,
+                "Slovakia" : 38,
+                "Slovenia" : 39,
+                "Spain" : 40,
+                "Sweden" : 41,
+                "Switzerland" : 42,
+                "Turkey" : 43,
+                "Ukraine" : 44,
+                "United Kingdom" : 45,
+                "Vatican" : 46,}
+file.close()
+#class MainWindow, setting up the ui (userinterface)
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+
+        self.setWindowTitle("Wikipedia")
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.eigenschaft_combo.addItem("All Data")
+        #routine for filling combo boxes with choices for data
+        with open("data/GeographicalEurope.csv", "r", newline="") as file: #"data/dateneuropa.txt"
+            first_line = file.readline().strip().split(";")
+            for choice in first_line:
+                if choice == "ï»¿Country":
+                    continue
+                self.ui.eigenschaft_combo.addItem(choice)
+
+        #routine for filling combo boxes with choices of countries
+        with open("data/GeographicalEurope.csv", "r", newline="") as file:
+            file.readline()
+            for line in file:
+                line_splitted = line.strip().split(";")
+                self.ui.land_combo.addItem(line_splitted[0])
+
+        self.ui.ok.clicked.connect(self.onOKclicked)
+        self.ui.abbr.clicked.connect(self.onAbbrclicked)
+        self.ui.close_button.clicked.connect(self.onCloseclicked)
+
+    def onOKclicked(self):
+        country = self.ui.land_combo.currentText()
+        self.ui.ausgabe_label.setText(country +":")
+        attr = self.ui.eigenschaft_combo.currentText()
+        self.ui.label_2.setText(attr +":")
+
+        if attr == "All Data":
+            self.ui.label_2.setText("Capitol, Area in km^2, Inhabitants, Inhabitants per km^2, BIP, BIP per capita, Region:")
+            self.ui.textBrowser.setPlainText(dp.at[country_codes[country], "Capitol"])
+            self.ui.textBrowser.append(dp.at[country_codes[country], "Area in km2"])
+            self.ui.textBrowser.append(dp.at[country_codes[country], "Inhabitants"])
+            self.ui.textBrowser.append(dp.at[country_codes[country], "Inhabitants per km2"])
+            self.ui.textBrowser.append(dp.at[country_codes[country], "BIP (bn USD, 2016)"])
+            self.ui.textBrowser.append(dp.at[country_codes[country], "BIP per capita (bn USD, 2016)"])
+            self.ui.textBrowser.append(dp.at[country_codes[country], "Region"])
+
+
+        elif attr == "Inhabitants":
+            self.ui.textBrowser.setPlainText(dp.at[country_codes[country], attr])
+        elif attr == "Area in km2":
+            self.ui.textBrowser.setPlainText(dp.at[country_codes[country], attr])
+        elif attr == "Capitol":
+            self.ui.textBrowser.setPlainText(dp.at[country_codes[country], attr])
+        elif attr == "Inhabitants per km2":
+            self.ui.textBrowser.setPlainText(dp.at[country_codes[country], attr])
+        elif attr == "BIP (bn USD, 2016)":
+            self.ui.textBrowser.setPlainText(dp.at[country_codes[country], attr])
+        elif attr == "BIP per capita (bn USD, 2016)":
+            self.ui.textBrowser.setPlainText(dp.at[country_codes[country], attr])
+        elif attr == "Region":
+            self.ui.textBrowser.setPlainText(dp.at[country_codes[country], attr])
+
+    def onAbbrclicked(self):
+        self.ui.ausgabe_label.clear()
+        self.ui.label_2.clear()
+        self.ui.textBrowser.clear()
+
+    def onCloseclicked(self):
+        self.close()
+
+window = MainWindow()
+window.show()
+sys.exit(app.exec_())
---
+++ ui/mainwindow.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui\mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1061, 674)
+        MainWindow.setStyleSheet("")
+        self.centralWidget = QtWidgets.QWidget(MainWindow)
+        self.centralWidget.setObjectName("centralWidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralWidget)
+        self.gridLayout.setContentsMargins(11, 11, 11, 11)
+        self.gridLayout.setSpacing(6)
+        self.gridLayout.setObjectName("gridLayout")
+        self.ausgabe_label = QtWidgets.QLabel(self.centralWidget)
+        self.ausgabe_label.setObjectName("ausgabe_label")
+        self.gridLayout.addWidget(self.ausgabe_label, 1, 0, 1, 1)
+        self.label_2 = QtWidgets.QLabel(self.centralWidget)
+        self.label_2.setObjectName("label_2")
+        self.gridLayout.addWidget(self.label_2, 1, 1, 1, 1)
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralWidget)
+        font = QtGui.QFont()
+        font.setPointSize(36)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.textBrowser.setFont(font)
+        self.textBrowser.setStyleSheet("")
+        self.textBrowser.setFrameShape(QtWidgets.QFrame.Box)
+        self.textBrowser.setObjectName("textBrowser")
+        self.gridLayout.addWidget(self.textBrowser, 2, 0, 1, 2)
+        self.formLayout = QtWidgets.QFormLayout()
+        self.formLayout.setHorizontalSpacing(9)
+        self.formLayout.setVerticalSpacing(17)
+        self.formLayout.setObjectName("formLayout")
+        self.land_label = QtWidgets.QLabel(self.centralWidget)
+        self.land_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.land_label.setObjectName("land_label")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.land_label)
+        self.land_combo = QtWidgets.QComboBox(self.centralWidget)
+        self.land_combo.setEditable(False)
+        self.land_combo.setCurrentText("")
+        self.land_combo.setMaxCount(2147483647)
+        self.land_combo.setObjectName("land_combo")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.land_combo)
+        self.eigenschaft_label = QtWidgets.QLabel(self.centralWidget)
+        self.eigenschaft_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.eigenschaft_label.setObjectName("eigenschaft_label")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.eigenschaft_label)
+        self.eigenschaft_combo = QtWidgets.QComboBox(self.centralWidget)
+        self.eigenschaft_combo.setObjectName("eigenschaft_combo")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.eigenschaft_combo)
+        self.ok = QtWidgets.QPushButton(self.centralWidget)
+        self.ok.setObjectName("ok")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.LabelRole, self.ok)
+        self.abbr = QtWidgets.QPushButton(self.centralWidget)
+        self.abbr.setObjectName("abbr")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.abbr)
+        self.gridLayout.addLayout(self.formLayout, 0, 0, 1, 1)
+        self.close_button = QtWidgets.QPushButton(self.centralWidget)
+        self.close_button.setStyleSheet("")
+        self.close_button.setObjectName("close_button")
+        self.gridLayout.addWidget(self.close_button, 3, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralWidget)
+        self.menuBar = QtWidgets.QMenuBar(MainWindow)
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 1061, 26))
+        self.menuBar.setObjectName("menuBar")
+        MainWindow.setMenuBar(self.menuBar)
+        self.mainToolBar = QtWidgets.QToolBar(MainWindow)
+        self.mainToolBar.setObjectName("mainToolBar")
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.mainToolBar)
+        self.statusBar = QtWidgets.QStatusBar(MainWindow)
+        self.statusBar.setObjectName("statusBar")
+        MainWindow.setStatusBar(self.statusBar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Wikipedia"))
+        self.ausgabe_label.setText(_translate("MainWindow", "TextLabel"))
+        self.label_2.setText(_translate("MainWindow", "TextLabel"))
+        self.textBrowser.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:36pt; font-weight:600; font-style:normal;\">\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:7.8pt; font-weight:400;\"><br /></p></body></html>"))
+        self.land_label.setText(_translate("MainWindow", "Country"))
+        self.eigenschaft_label.setText(_translate("MainWindow", "Feature"))
+        self.ok.setText(_translate("MainWindow", "OK"))
+        self.abbr.setText(_translate("MainWindow", "Cancel"))
+        self.close_button.setText(_translate("MainWindow", "Close"))
+
+
